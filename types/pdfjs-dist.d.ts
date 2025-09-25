5ee47d1d571ad00691084745a81e95cba26b5d54
bpensione <bpensione@gmail.com>
2025-09-25 18:42:50 -0300

    Create pdfjs-dist.d.ts

---
+++ types/pdfjs-dist.d.ts
@@ -0,0 +1,5 @@
+// Tipagem mínima para o import ESM do pdfjs-dist usado no /check
+declare module 'pdfjs-dist/build/pdf.mjs' {
+  export const getDocument: any;
+  export const GlobalWorkerOptions: { workerSrc: string };
+}
